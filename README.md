f030349556fbdfbe64cc6946db861e1c6fe02c29
ichou <me@ichou.cn>
2014-09-01 22:08:44 +0800

    ver 2.0

--- README.md
+++ README.md
@@ -1,29 +1,43 @@
-##基于阿里云OSS的WordPress远程附件支持插件——阿里云附件(Aliyun Support)(修订版)
+## 基于阿里云OSS的WordPress远程附件支持插件——阿里云附件(Aliyun Support)(修订版)
 
 原插件地址：http://mawenjian.net/p/977.html
 
 由于原插件作者没有持续更新，已经无法使用（或无法兼容最新版本环境），故对此进行一些小修正
+____________________
+
+修正日期：2014-9-1
+
+** 版本号：2.0 **
+
+### 修订内容：
+1. 完全重构，优化代码
+2. 支持 Aliyun OSS 的图片服务
+3. 改变钩子嵌入机制，支持所有附件（以前的版本只上传图片，而且启用时其他附件完全不可用了，坑！！）
+4. 添加卸载、不残留
+
+
+____________________
 
 修正日期：2014-8-27
 
-版本号：1.1
+** 版本号：1.1 **
 
-###修订项目：
+### 修订项目：
 1. 插件年久失修，其内部调用的 Aliyun OSS php SDK 已升级
 2. WordPress 3.5以后 设置->多媒体 中没有路径配置，导致配置不便
 3. Aliyun OSS 可以绑定自己的域名，插件中不能简单的设置
 
-###修订内容：
+### 修订内容：
 1. 升级 Aliyun-OSS-SDK 到 1.1.6 版本 (2014-06-25更新)
 2. 设置中可直接配置访问路径 Url，支持已绑定到 OSS 的独立域名
 3. 支持自定义 OSS 上文件的存放目录 （不影响本地存储，中途若修改请手动移动 OSS 上文件，否则可能链接不到之前的资源）
 4. 修正原插件 bug 若干
 5. 优化代码 （移除所有 Notice 级报错）
 
-###TODO:
+### TODO:
 原作者的代码在 github 上托管了一份，是不是应该联系原作者进行更新
 
-###插件下载：
+### 插件下载：
 [OSS-Support.zip](https://github.com/IvanChou/aliyun-oss-support/archive/master.zip)
 
 [发布地址](http://ichou.cn/posts/ji-yu-a-li-yun-ossde-wordpressyuan-cheng-fu-jian-zhi-chi-cha-jian-a-li-yun-fu-jian-aliyun-support-xiu-ding-ban)
--- oss-support.php
+++ oss-support.php
@@ -3,13 +3,20 @@
  * Plugin Name: 阿里云附件
  * Plugin URI: "http://mawenjian.net/p/977.html"
  * Description: 使用阿里云存储OSS作为附件存储空间。This is a plugin that used Aliyun Cloud Storage(Aliyun OSS) for attachments remote saving.
- * Author: 马文建(Wenjian Ma) & ichou
- * Version: 1.1
- * Author URI: http://mawenjian.net/
+ * Author: Ivan Chou
+ * Author URI: http://ichou.cn/
+ * Original Author: 马文建(Wenjian Ma)
+ * Version: 2.0
  */
 
 
 /**
+ *   ==== ver: 1.0 ====
+ * Plugin URI: "http://mawenjian.net/p/977.html"
+ * Author: 马文建(Wenjian Ma)
+ * Author URI: http://mawenjian.net/
+ *
+ *   ==== ver: 1.1 ====
  * iChou升级说明
  * Update URI: http://ichou.cn/posts/ji-yu-a-li-yun-ossde-wordpressyuan-cheng-fu-jian-zhi-chi-cha-jian-a-li-yun-fu-jian-aliyun-support-xiu-ding-ban
  * Author: Ivan Chou (ichou.cn)
@@ -20,36 +27,43 @@
  * 4.修正原插件 bug 若干
  * 5.优化代码 （移除所有 Notice 级报错）
  *
+ *   ==== ver: 2.0 ====
+ * Author: Ivan Chou (ichou.cn)
+ * 1.完全重构
+ * 2.支持 Aliyun OSS 的图片服务
+ * 3.改变钩子嵌入机制，支持所有附件（以前的版本只有图片，而且启用时其他附件完全不可用了，坑！！）
+ *
  */
 
 require_once('sdk.class.php');
-if ( !defined('WP_PLUGIN_URL') )
-	define( 'WP_PLUGIN_URL', WP_CONTENT_URL . '/plugins' );
 
 //  plugin url
-define('OSS_BASENAME', plugin_basename(__FILE__));
 define('OSS_BASEFOLDER', plugin_basename(dirname(__FILE__)));
-define('OSS_FILENAME', str_replace(OSS_BASEFOLDER.'/', '', plugin_basename(__FILE__)));
 
-// 初始化
-register_activation_hook(__FILE__, 'oss_set_options');
+$oss_options = get_option('oss_options');
 
-// 初始化选项
+/**
+ * 初始化
+ */
 function oss_set_options() {
     $options = array(
         'bucket' => "",
         'ak' => "",
     	'sk' => "",
         'path' => "",
-        'cname' => "",
+        'static_url' => "",
+        'img_url' => ""
     );
-    
     add_option('oss_options', $options, '', 'yes');
 }
+register_activation_hook(__FILE__, 'oss_set_options');
 
-
-function oss_admin_warnings() {
-    $oss_options = get_option('oss_options');
+/**
+ * 设置提醒
+ *
+ * @param $oss_options
+ */
+function oss_admin_warnings($oss_options) {
 
     $oss_bucket = isset($oss_options['bucket']) ? esc_attr($oss_options['bucket']) : null;
 	if ( !$oss_bucket && !isset($_POST['submit']) ) {
@@ -60,201 +74,250 @@ function oss_admin_warnings() {
 		return;
 	} 
 }
-oss_admin_warnings();
-
-//上传函数
-function _file_upload( $object , $file , $opt = array()){
-		
-	//获取WP配置信息
-	$oss_options = get_option('oss_options', TRUE);
-    $oss_bucket = esc_attr($oss_options['bucket']);
-	$oss_ak = esc_attr($oss_options['ak']);
-	$oss_sk = esc_attr($oss_options['sk']);
-
-    if ($oss_options['path'] != "")
-        $object = $oss_options['path'].$object;
-
-	//实例化存储对象
-	if(!is_object($aliyun_oss))
-		$aliyun_oss = new ALIOSS($oss_ak, $oss_sk);
-	//上传原始文件
-	$opt['Expires'] = 'access plus 1 years';
-	$aliyun_oss->upload_file_by_file( $oss_bucket, $object, $file, $opt );
-
-	return TRUE;
-}
-
-//删除本地文件
-function _delete_local_file($file){
-	try{
-	  //文件不存在
-		if(!@file_exists($file))
-			return TRUE;
-		//删除文件
-		if(!@unlink($file))
-			return FALSE;
-		return TRUE;
-	}
-	catch(Exception $ex){
-		return FALSE;
-	}
-}
+oss_admin_warnings($oss_options);
 
 /**
- * 上传所有文件到服务器，没有删除本地文件
-*  @static
- * @param $metadata from function wp_generate_attachment_metadata
- * @return array
+ * 上传原文件到 OSS (并根据设定清理本地文件)
+ *
+ * @param $file
+ * @return mixed
  */
-function upload_images($metadata)
-{	
-	//获取上传路径
-	$wp_upload_dir = wp_upload_dir();
-	$oss_options = get_option('oss_options', TRUE);
-	$oss_nolocalsaving = (esc_attr($oss_options['nolocalsaving'])=='true') ? true : false;
+function upload_orign_2_oss($file)
+{
+    $wp_uploads = wp_upload_dir();
+    $oss_options = get_option('oss_options', TRUE);
+    $oss_bucket = esc_attr($oss_options['bucket']);
+    $oss_ak = esc_attr($oss_options['ak']);
+    $oss_sk = esc_attr($oss_options['sk']);
+    $oss_upload_path = trim($oss_options['path'],'/');
+    $oss_nolocalsaving = (esc_attr($oss_options['nolocalsaving'])=='true') ? true : false;
 
-	$upload_path = get_option('upload_path');
-	if($upload_path == '.' ){
-		$upload_path = '';
-		$object = $metadata['file'];
-	}
-	else{
-		$upload_path = trim($upload_path,'/');
-		$object = ltrim($upload_path.'/'.$metadata['file'],'/');
-	}
-	
-	//上传原始文件
-	$file = $wp_upload_dir['basedir'].'/'.$metadata['file'];
-	_file_upload ( $object, $file);
-	
-	//如果不在本地保存，则删除
-	if($oss_nolocalsaving)
-		_delete_local_file($file);
+    $object = str_replace($wp_uploads['basedir'], '', $file['file']);
+    $object = ltrim($oss_upload_path . '/' .ltrim($object, '/'), '/');
 
-	//上传小尺寸文件
-	if (isset($metadata['sizes']) && count($metadata['sizes']) > 0)
-	{
-		//there may be duplicated filenames,so ....
-		foreach ($metadata['sizes'] as $val)
-		{
-			$object = ltrim( $upload_path.$wp_upload_dir['subdir'].'/'.$val['file'] , '/' );
-			$file = $wp_upload_dir['path'].'/'.$val['file'];
-			$opt =array('Content-Type' => $val['mime-type']);
-			_file_upload ( $object, $file, $opt );
-			
-			//如果不在本地保存，则删除
-			if($oss_nolocalsaving)
-				_delete_local_file($file);
+    if(!is_object($aliyun_oss))
+        $aliyun_oss = new ALIOSS($oss_ak, $oss_sk);
 
-		}
-	}
-	return $metadata;
+    $opt['Expires'] = 'access plus 1 years';
+    $aliyun_oss->upload_file_by_file( $oss_bucket, $object, $file['file'], $opt );
+
+    if($oss_nolocalsaving && false === strpos($file['type'], 'image')){
+        _delete_local_file($file['file']);
+    }
+
+    return $file;
+
 }
+add_filter('wp_handle_upload', 'upload_orign_2_oss', 30);
+
+/**
+ * 上传生成的缩略图到 OSS (并根据设定清理本地文件)
+ *
+ * @param $metadata
+ * @return mixed
+ */
+function upload_thumb_2_oss($metadata)
+{
+    $wp_uploads = wp_upload_dir();
+    $oss_options = get_option('oss_options', TRUE);
+    $oss_bucket = esc_attr($oss_options['bucket']);
+    $oss_ak = esc_attr($oss_options['ak']);
+    $oss_sk = esc_attr($oss_options['sk']);
+    $oss_upload_path = trim($oss_options['path'],'/');
+    $oss_nolocalsaving = (esc_attr($oss_options['nolocalsaving'])=='true') ? true : false;
+
+    if(!is_object($aliyun_oss) && $oss_options['img_url'] == "")
+        $aliyun_oss = new ALIOSS($oss_ak, $oss_sk);
+    $opt['Expires'] = 'access plus 1 years';
+
+    if (isset($metadata['sizes']) && count($metadata['sizes']) > 0) {
+        foreach ($metadata['sizes'] as $val) {
+            $object = ltrim($oss_upload_path . '/' .trim($wp_uploads['subdir'], '/').'/'.ltrim($val['file'], '/'), '/');
+            $file = $wp_uploads['path'].'/'.$val['file'];
+
+            if($oss_options['img_url'] == "")
+                $aliyun_oss->upload_file_by_file( $oss_bucket, $object, $file, $opt );
+
+            if($oss_nolocalsaving)
+                _delete_local_file($file);
+        }
+    }
+
+    if($oss_nolocalsaving) {
+        $file = $wp_uploads['basedir'].'/'.$metadata['file'];
+        _delete_local_file($file);
+    }
+
+    return $metadata;
+}
+add_filter('wp_generate_attachment_metadata', 'upload_thumb_2_oss', 60);
 
 /**
  * 删除远程服务器上的单个文件
- * @static
+ * 
  * @param $file
- * @return void
+ * @return mixed
  */
 function delete_remote_file($file)
-{		
-	//获取WP配置信息
-	$oss_options = get_option('oss_options', TRUE);
+{
+    if(!false == strpos($file, '@!'))
+        return $file;
+
+    $oss_options = get_option('oss_options', TRUE);
     $oss_bucket = esc_attr($oss_options['bucket']);
-	$oss_ak = esc_attr($oss_options['ak']);
-	$oss_sk = esc_attr($oss_options['sk']);
-	
-	//获取保存路径
-	$upload_path = get_option('upload_path');
-	if($upload_path == '.' )
-		$upload_path='';
-	else
-		$upload_path = trim($upload_path,'/');
-	
-	//获取上传路径
-	$wp_upload_dir = wp_upload_dir();
-	
-	$del_file = str_replace($wp_upload_dir['basedir'],'',$file);
-	$del_file = ltrim( str_replace('./','',$del_file), '/');
-	if( $upload_path != '' )
-		$del_file = $upload_path .'/'. $del_file;
+    $oss_ak = esc_attr($oss_options['ak']);
+    $oss_sk = esc_attr($oss_options['sk']);
+    $oss_upload_path = trim($oss_options['path'],'/');
+    $wp_uploads = wp_upload_dir();
 
-    if ($oss_options['path'] != "")
-        $del_file = $oss_options['path'].$del_file;
-	
-	//实例化存储对象
-	if(!is_object($aliyun_oss))
-		$aliyun_oss = new ALIOSS($oss_ak, $oss_sk);
-	//删除文件
-	$aliyun_oss->delete_object( $oss_bucket, $del_file);
+    $del_file = str_replace($wp_uploads['basedir'], '', $file);
+    $del_file = ltrim($oss_upload_path . '/' .ltrim($del_file, '/'), '/');
 
-	return $file;
+    if(!is_object($aliyun_oss))
+        $aliyun_oss = new ALIOSS($oss_ak, $oss_sk);
+
+    $aliyun_oss->delete_object( $oss_bucket, $del_file);
+
+    return $file;
 }
-
-//生成缩略图后立即上传生成的文件
-add_filter('wp_generate_attachment_metadata', 'upload_images', 999);
-//删除远程附件
 add_action('wp_delete_file', 'delete_remote_file');
 
-function oss_plugin_action_links( $links, $file ) {
-	if ( $file == plugin_basename( dirname(__FILE__).'/oss-support.php' ) ) {
-		$links[] = '<a href="options-general.php?page=' . OSS_BASEFOLDER . '/oss-support.php">'.__('Settings').'</a>';
-	}
+/**
+ * 删除本地的缩略图（修正由于启用图片服务导致的原生方法删除不了缩略图）
+ * 仅在开启图片服务时启用
+ * 
+ * @param $file
+ * @return mixed
+ */
+function delete_thumb_img($file)
+{
+    if(!false == strpos($file, '@!')) //todo
+        return $file;
 
-	return $links;
+    $file_t = substr($file, 0, strrpos($file, '.'));
+    array_map('_delete_local_file', glob($file_t.'-*'));
+    return $file;
 }
+if(!$oss_options['img_url'] == "")
+    add_action('wp_delete_file', 'delete_thumb_img', 99);
 
+/**
+ * 修改从数据库中取出的图片信息，以使用 Aliyun 的图片服务
+ * 仅在开启图片服务时启用
+ *
+ * @param $data
+ * @return mixed
+ */
+function modefiy_img_meta($data) {
+    $filename = basename($data['file']);
+
+    if(isset($data['sizes']['thumbnail'])) {
+        $data['sizes']['thumbnail']['file'] = $filename.'@!thumbnail';
+    }
+    if(isset($data['sizes']['post-thumbnail'])) {
+        $data['sizes']['post-thumbnail']['file'] = $filename.'@!post-thumbnail';
+    }
+    if(isset($data['sizes']['medium'])) {
+        $data['sizes']['medium']['file'] = $filename.'@!medium';
+    }
+    if(isset($data['sizes']['large'])) {
+        $data['sizes']['large']['file'] = $filename.'@!large';
+    }
+
+    return $data;
+}
+if(!$oss_options['img_url'] == "")
+    add_filter('wp_get_attachment_metadata', 'modefiy_img_meta', 990);
+
+/**
+ * 重置图片链接，使用独立的图片服务器。
+ * 仅在开启图片服务时启用
+ *
+ * @param $url
+ * @return mixed
+ */
+function modefiy_img_url($url, $post_id) {
+    $wp_uploads = wp_upload_dir();
+    $oss_options = get_option('oss_options', TRUE);
+
+    if(wp_attachment_is_image($post_id)){
+        $img_baseurl = rtrim($oss_options['img_url'], '/') .'/'. rtrim($oss_options['path'], '/');
+        $url = str_replace(rtrim($wp_uploads['baseurl'], '/'), $img_baseurl, $url);
+    }
+    return $url;
+}
+if(!$oss_options['img_url'] == "")
+    add_filter('wp_get_attachment_url', 'modefiy_img_url', 30, 2);
+
+
+/**
+ * 设置 upload_url_path，使用外部存储OSS
+ *
+ * @param $uploads
+ * @return mixed
+ */
+function reset_upload_url_path( $uploads ) {
+    $oss_options = get_option('oss_options', TRUE);
+
+    if ($oss_options['static_url'] != "") {
+        $baseurl = rtrim($oss_options['static_url'], '/') .'/'. rtrim($oss_options['path'], '/');
+        $uploads['baseurl'] = $baseurl;
+    }
+    return $uploads;
+}
+add_filter( 'upload_dir', 'reset_upload_url_path', 30 );
+
+/**
+ * 添加设置页面入口连接
+ */
+function oss_plugin_action_links( $links, $file ) {
+    if ( $file == plugin_basename( dirname(__FILE__).'/oss-support.php' ) ) {
+        $links[] = '<a href="options-general.php?page=' . OSS_BASEFOLDER . '/oss-support.php">'.__('Settings').'</a>';
+    }
+    return $links;
+}
 add_filter( 'plugin_action_links', 'oss_plugin_action_links', 10, 2 );
 
+/**
+ * 设置页面
+ */
 function oss_add_setting_page() {
     add_options_page('OSS Setting', 'OSS Setting', 'manage_options', __FILE__, 'oss_setting_page');
 }
-
 add_action('admin_menu', 'oss_add_setting_page');
-
-//设置 upload_url_path 地址     by:ichou 08-27-2014
-function oss_setting_url( $uploads ) {
-    $oss_options = get_option('oss_options', TRUE);
-
-    if ($oss_options['cname'] != "") {
-        $baseurl = rtrim($oss_options['cname'], '/') .'/'. rtrim($oss_options['path'], '/');
-        $uploads['baseurl'] = $baseurl;
-        return $uploads;
-    }
-}
-
-add_filter( 'upload_dir', 'oss_setting_url' );
 
 function oss_setting_page() {
 
-	$options = array();
-	if(isset($_POST['bucket'])) {
-		$options['bucket'] = trim(stripslashes($_POST['bucket']));
-	}
-	if(isset($_POST['ak'])) {
-		$options['ak'] = trim(stripslashes($_POST['ak']));
-	}
-	if(isset($_POST['sk'])) {
-		$options['sk'] = trim(stripslashes($_POST['sk']));
-	}
+    $options = array();
+    if(isset($_POST['bucket'])) {
+        $options['bucket'] = trim(stripslashes($_POST['bucket']));
+    }
+    if(isset($_POST['ak'])) {
+        $options['ak'] = trim(stripslashes($_POST['ak']));
+    }
+    if(isset($_POST['sk'])) {
+        $options['sk'] = trim(stripslashes($_POST['sk']));
+    }
     if(isset($_POST['path'])) {
         $options['path'] = rtrim(trim(stripslashes($_POST['path'])), '/').'/';
     }
-    if(isset($_POST['cname'])) {
-        $options['cname'] = trim(stripslashes($_POST['cname']));
+    if(isset($_POST['static_url'])) {
+        $options['static_url'] = trim(stripslashes($_POST['static_url']));
+    }
+    if(isset($_POST['img_url'])) {
+        $options['img_url'] = trim(stripslashes($_POST['img_url']));
     }
     if(isset($_POST['nolocalsaving'])) {
         $options['nolocalsaving'] = 'true';
     }
 
-	if($options !== array() ){
-	
-		update_option('oss_options', $options);
-        
-?>
-<div class="updated"><p><strong>设置已保存！</strong></p></div>
-<?php
+    if($options !== array() ){
+
+        update_option('oss_options', $options);
+
+        ?>
+        <div class="updated"><p><strong>设置已保存！</strong></p></div>
+    <?php
     }
 
     $oss_options = get_option('oss_options');
@@ -263,48 +326,83 @@ function oss_setting_page() {
     $oss_ak = isset($oss_options['ak']) ? esc_attr($oss_options['ak']) : null;
     $oss_sk = isset($oss_options['sk']) ? esc_attr($oss_options['sk']) : null;
     $oss_path = isset($oss_options['path']) ? esc_attr($oss_options['path']) : null;
-    $oss_cname = isset($oss_options['cname']) ? esc_attr($oss_options['cname']) : null;
-	
-	$oss_nolocalsaving = isset($oss_options['nolocalsaving']) ? esc_attr($oss_options['nolocalsaving']) : null;
-	($oss_nolocalsaving == 'true') ? ($oss_nolocalsaving = true) : ($oss_nolocalsaving = false);
-?>
-<div class="wrap" style="margin: 10px;">
-    <h2>阿里云存储 设置</h2>
-    <form name="form1" method="post" action="<?php echo wp_nonce_url('./options-general.php?page=' . OSS_BASEFOLDER . '/oss-support.php'); ?>">
-        <fieldset>
-            <legend>Bucket 设置</legend>
-            <input type="text" name="bucket" value="<?php echo $oss_bucket;?>" placeholder="请输入云存储使用的 bucket"/>
-            <p>请先访问 <a href="http://i.aliyun.com/dashboard?type=oss">阿里云存储</a> 创建 bucket 后，填写以上内容。</p>
-        </fieldset>
-        <fieldset>
-            <legend>Access Key / API key</legend>
-            <input type="text" name="ak" value="<?php echo $oss_ak;?>" placeholder=""/>
-            <p>访问 <a href="http://i.aliyun.com/access_key/" target="_blank">阿里云 密钥管理页面</a>，获取 AKSK</p>
-        </fieldset>
-        <fieldset>
-            <legend>Secret Key</legend>
-            <input type="text" name="sk" value="<?php echo $oss_sk;?>" placeholder=""/>
-            <P> </P>
-        </fieldset>
-        <fieldset>
-            <legend>Save path on OSS</legend>
-            <input type="text" name="path" value="<?php echo $oss_path;?>" placeholder=""/>
-            <P></P>
-        </fieldset>
-        <fieldset>
-            <legend>OSS-Url</legend>
-            <input type="text" name="cname" value="<?php echo $oss_cname;?>" placeholder=""/>
-            <P>OSS 的可访问URL 支持已绑定到 OSS 的独立域名</P>
-        </fieldset>
-        <fieldset>
-            <legend>不在本地保留备份：</legend>
-            <input type="checkbox" name="nolocalsaving" <?php if($oss_nolocalsaving) echo 'checked="TRUE"';?> />
-        </fieldset>
-        <fieldset class="submit">
-            <legend>更新选项</legend>
-            <input type="submit" name="submit" value="更新" />
-        </fieldset>
-    </form>
-</div>
+    $oss_static_url = isset($oss_options['static_url']) ? esc_attr($oss_options['static_url']) : null;
+    $oss_img_url = isset($oss_options['img_url']) ? esc_attr($oss_options['img_url']) : null;
+
+    $oss_nolocalsaving = isset($oss_options['nolocalsaving']) ? esc_attr($oss_options['nolocalsaving']) : null;
+    ($oss_nolocalsaving == 'true') ? ($oss_nolocalsaving = true) : ($oss_nolocalsaving = false);
+    ?>
+    <div class="wrap" style="margin: 10px;">
+        <h2>阿里云存储 设置</h2>
+        <form name="form1" method="post" action="<?php echo wp_nonce_url('./options-general.php?page=' . OSS_BASEFOLDER . '/oss-support.php'); ?>">
+            <hr>
+            <fieldset>
+                <legend>Bucket 设置</legend>
+                <input type="text" name="bucket" value="<?php echo $oss_bucket;?>" placeholder="请输入云存储使用的 bucket"/>
+                <p>请先访问 <a href="http://i.aliyun.com/dashboard?type=oss">阿里云存储</a> 创建 bucket 后，填写以上内容。</p>
+            </fieldset>
+            <hr>
+            <fieldset>
+                <legend>Access Key / API key</legend>
+                <input type="text" name="ak" value="<?php echo $oss_ak;?>" placeholder=""/>
+            </fieldset>
+            <br>
+            <fieldset>
+                <legend>Secret Key</legend>
+                <input type="text" name="sk" value="<?php echo $oss_sk;?>" placeholder=""/>
+                <p>访问 <a href="http://i.aliyun.com/access_key/" target="_blank">阿里云 密钥管理页面</a>，获取 AKSK</p>
+            </fieldset>
+            <hr>
+            <fieldset>
+                <legend>Save path on OSS</legend>
+                <input type="text" name="path" value="<?php echo $oss_path;?>" placeholder=""/>
+                <P>可以设置在 OSS 上存储的位置，如果你要存在根目录就留空，我真的没意见(ﾉ￣д￣)ﾉ</P>
+            </fieldset>
+            <fieldset>
+                <legend>OSS-Http-Url</legend>
+                <input type="text" name="static_url" value="<?php echo $oss_static_url;?>" placeholder="http://"/>
+                <P>OSS Bucket 的可访问 URL，支持已绑定到 OSS 的独立域名，留空将使用本地资源</P>
+            </fieldset>
+            <hr>
+            <fieldset>
+                <legend>Aliyun-OSS 图片服务的 URL</legend>
+                <input type="text" name="img_url" value="<?php echo $oss_img_url;?>" placeholder="http://"/>
+                <dl>
+                    <dt>请瞩目：</dt>
+                    <dd>1.图片服务是可选的，留空即可不启用</dd>
+                    <dd>2.使用请先在Aliyun中设置好四种样式: <code>{'thumbnail','post-thumbnail','large','medium'}</code></dd>
+                    <dd>3.开启图片服务后，只有原图会上传到 OSS 中，缩略图不会再上传</dd>
+                </dl>
+            </fieldset>
+            <hr>
+            <fieldset>
+                <label><input type="checkbox" name="nolocalsaving" <?php if($oss_nolocalsaving) echo 'checked="TRUE"';?> /> 不在本地保留备份</label>
+            </fieldset>
+            <fieldset class="submit">
+                <input type="submit" name="submit" value="更新" />
+            </fieldset>
+        </form>
+    </div>
 <?php
+}
+
+/**
+ * 删除本地的文件
+ *
+ * @param $file
+ * @return bool
+ */
+function _delete_local_file($file){
+    try{
+        //文件不存在
+        if(!@file_exists($file))
+            return TRUE;
+        //删除文件
+        if(!@unlink($file))
+            return FALSE;
+        return TRUE;
+    }
+    catch(Exception $ex){
+        return FALSE;
+    }
 }
