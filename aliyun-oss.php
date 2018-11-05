857508275eafa05fba48184cb5410952fc39644c
IChou <me@ichou.cn>
2018-11-06 01:40:35 +0800

    bump 3.2.3

--- aliyun-oss.php
+++ aliyun-oss.php
@@ -4,8 +4,8 @@
  * Description: 使用阿里云 OSS 作为附件的存储空间。 This is a plugin that used Aliyun OSS for attachments remote saving.
  * Author: Ivan Chou
  * Author URI: https://yii.im/
- * Version: 3.2.2
- * Updated_at: 2018-08-21
+ * Version: 3.2.3
+ * Updated_at: 2018-11-06
  */
 
 /*  Copyright 2016  Ivan Chou  (email : yiichou@gmail.com)
